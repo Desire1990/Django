0f784afa03390a0e6e47f043259a00853451c7e1
Desos_Wajos <desnduwim26@gmail.com>
2020-02-21 14:15:15 +0200

    comment in settings drf permission

--- api_test/mysite/mysite/settings.py
+++ api_test/mysite/mysite/settings.py
@@ -34,11 +34,11 @@ INSTALLED_APPS = [
     'books',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':
-'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES':
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     
-}
+# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
